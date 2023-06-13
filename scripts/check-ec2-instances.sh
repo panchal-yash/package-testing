f29b0c628504d71b6aa8806a6fad74cbc5c35e35
Yash Panchal <46398416+panchal-yash@users.noreply.github.com>
2023-06-13 18:56:27 +0530

    Update check-ec2-instances.sh

--- scripts/check-ec2-instances.sh
+++ scripts/check-ec2-instances.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 #
 ## Calculate the threshold date (1 day ago)
-threshold_date=$(date -d "$2 day ago" +%Y-%m-%d)
-#
-## Retrieve the list of EC2 instances
-aws ec2 describe-instances     --filters "Name=instance-state-name,Values=running"     --query "Reservations[].Instances[?LaunchTime<='${threshold_date}'].[Tags[?Key=='Name'].Value[], InstanceId, LaunchTime]" --output yaml --region $1 | sed -e 's/\- \[\]//g' -e '/^$/d'
+awsRegions=("us-west-1" "us-west-2")
+
+
+
+checkec2(){
+          threshold_date=$(date -d "$2 day ago" +%Y-%m-%d)
+
+            aws ec2 describe-instances     --filters "Name=instance-state-name,Values=running"     --query "Reservations[].Instances[?LaunchTime<='${threshold_date}'].[Tags[?Key=='Name'].Value[], InstanceId, LaunchTime]" --output yaml --region $1 | sed -e 's/\- \[\]//g' -e '/^$/d'
+    }
+
+
+
+    #
+    days="3"
+    for region in "${awsRegions[@]}"
+    do
+    #
+    #
+            echo "----------------"$region"----------------------"
+              checkec2 "$region" "$days"
+            echo "----------------------------------------------"
+    done
