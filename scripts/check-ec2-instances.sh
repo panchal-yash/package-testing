d44008330164f7ae59f26248861d16150e939e2f
Yash Panchal <46398416+panchal-yash@users.noreply.github.com>
2023-06-13 16:44:28 +0530

    Update check-ec2-instances.sh

--- scripts/check-ec2-instances.sh
+++ scripts/check-ec2-instances.sh
@@ -4,6 +4,4 @@
 threshold_date=$(date -d "$2 day ago" +%Y-%m-%d)
 #
 ## Retrieve the list of EC2 instances
-aws ec2 describe-instances \
-  --filters "Name=instance-state-name,Values=running" \
-  --query "Reservations[].Instances[?LaunchTime<='${threshold_date}'].[Tags[?Key=='Name'].Value[], InstanceId, LaunchTime]" --region $1
+aws ec2 describe-instances     --filters "Name=instance-state-name,Values=running"     --query "Reservations[].Instances[?LaunchTime<='${threshold_date}'].[Tags[?Key=='Name'].Value[], InstanceId, LaunchTime]" --output yaml --region $1 | sed -e 's/\- \[\]//g' -e '/^$/d'
