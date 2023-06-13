0ea46fa295c3b2d684a4d649864b7aaeff86dc88
Yash Panchal <46398416+panchal-yash@users.noreply.github.com>
2023-06-13 16:08:38 +0530

    Create check.sh
    ec2-instance-checks check.sh script

---
+++ scripts/check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+## Calculate the threshold date (1 day ago)
+threshold_date=$(date -d "$2 day ago" +%Y-%m-%d)
+#
+## Retrieve the list of EC2 instances
+aws ec2 describe-instances \
+  --filters "Name=instance-state-name,Values=running" \
+  --query "Reservations[].Instances[?LaunchTime<='${threshold_date}'].[Tags[?Key=='Name'].Value[], InstanceId, LaunchTime]" --region $1
