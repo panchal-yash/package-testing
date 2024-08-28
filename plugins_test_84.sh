38ecf3853086e1673eea231b5291f726b6f03cb7
panchal-yash <yash.panchal@percona.com>
2024-08-28 20:05:26 +0530

    add component_uuid_vx_udf for ps84

--- plugins_test_84.sh
+++ plugins_test_84.sh
@@ -42,7 +42,7 @@ fi
 
 mysql -e "INSTALL PLUGIN clone SONAME 'mysql_clone.so';"
 
-for component in component_validate_password component_log_sink_syseventlog component_log_sink_json component_log_filter_dragnet component_audit_api_message_emit component_binlog_utils_udf component_percona_udf component_keyring_vault; do
+for component in component_validate_password component_log_sink_syseventlog component_log_sink_json component_log_filter_dragnet component_audit_api_message_emit component_binlog_utils_udf component_percona_udf component_keyring_vault component_uuid_vx_udf; do 
   if [ $(mysql -Ns -e "select count(*) from mysql.component where component_urn=\"file://${component}\";") -eq 0 ]; then
     mysql -e "INSTALL COMPONENT \"file://${component}\";"
   fi
